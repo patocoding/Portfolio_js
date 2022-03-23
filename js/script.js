072c83ce6ee98ab41ae3084affaa84e2de6aeaf2
cayo bruno <cayocomy1@gmail.com>
2022-03-23 16:03:09 -0300

    Portfolio - Initial
    Parte inicial do portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function () {
+    document.querySelector('.container').classList.toggle('show-menu');
+
+});
+
+
